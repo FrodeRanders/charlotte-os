0fbcbb51f5b8fe12c81bb140b6d39fbd7d8e9a24
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-03-06 22:36:01 -0500

    Remove context switch assembly macros and abstract timer information logging into a function under isa::timers so there is nothing ISA related in main.rs
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- crates/catten/src/cpu/isa/x86_64/timers/mod.rs
+++ crates/catten/src/cpu/isa/x86_64/timers/mod.rs
@@ -3,3 +3,19 @@ pub mod i8254;
 pub mod tsc;
 
 pub use apic_timer::LpTimer;
+
+use crate::cpu::isa::timers::tsc::{IS_TSC_INVARIANT, TSC_CYCLE_PERIOD, TSC_FREQUENCY_HZ};
+use crate::logln;
+
+pub fn print_timer_info() {
+    if *IS_TSC_INVARIANT {
+        logln!("The x86-64 Timestamp Counter IS invariant.");
+    } else {
+        logln!("The x86-64 Timestamp Counter is NOT invariant.");
+    }
+    logln!("The x86-64 Timestamp Counter frequency is {:?} MHz.", (*TSC_FREQUENCY_HZ / 1_000_000));
+    logln!(
+        "The x86-64 Timestamp Counter period is {:?} picoseconds.",
+        ((*TSC_CYCLE_PERIOD).as_picos())
+    );
+}
--- crates/catten/src/main.rs
+++ crates/catten/src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 #![feature(abi_custom)]
 #![feature(allocator_api)]
-#![feature(exclusive_wrapper)]
 #![feature(extend_one)]
 #![feature(iter_advance_by)]
 #![feature(likely_unlikely)]
@@ -39,16 +38,13 @@ pub mod memory;
 pub mod panic;
 pub mod self_test;
 
-use core::sync::atomic::{AtomicU8, Ordering};
-
 use limine::mp::Cpu;
 use spin::{Barrier, Lazy};
 
 use crate::cpu::isa::interface::system_info::CpuInfoIfce;
 use crate::cpu::isa::lp::ops::get_lp_id;
 use crate::cpu::isa::system_info::CpuInfo;
-#[cfg(target_arch = "x86_64")]
-use crate::cpu::isa::timers::tsc::{IS_TSC_INVARIANT, TSC_CYCLE_PERIOD, TSC_FREQUENCY_HZ};
+use crate::cpu::isa::timers::print_timer_info;
 use crate::cpu::multiprocessor::get_lp_count;
 use crate::cpu::multiprocessor::startup::{assign_id, start_secondary_lps};
 use crate::cpu::scheduler::system_scheduler::SYSTEM_SCHEDULER;
@@ -80,27 +76,12 @@ pub extern "C" fn bsp_main() -> ! {
     start_secondary_lps().expect("Failed to start secondary LPs");
     INIT_BARRIER.wait();
     self_test::run_self_tests();
-    #[cfg(target_arch = "x86_64")]
-    {
-        if *IS_TSC_INVARIANT {
-            logln!("The x86-64 Timestamp Counter IS invariant.");
-        } else {
-            logln!("The x86-64 Timestamp Counter is NOT invariant.");
-        }
-        logln!(
-            "The x86-64 Timestamp Counter frequency is {:?} MHz.",
-            (*TSC_FREQUENCY_HZ / 1_000_000)
-        );
-        logln!(
-            "The x86-64 Timestamp Counter period is {:?} picoseconds.",
-            ((*TSC_CYCLE_PERIOD).as_picos())
-        );
-    }
     logln!("System Information:");
     logln!("CPU Vendor: {}", (CpuInfo::get_vendor()));
     logln!("CPU Model: {}", (CpuInfo::get_model()));
     logln!("Physical Address bits implemented: {}", (CpuInfo::get_paddr_sig_bits()));
     logln!("Virtual Address bits implemented: {}", (CpuInfo::get_vaddr_sig_bits()));
+    print_timer_info();
     mask_interrupts!();
     for _ in 0..get_lp_count() {
         logln!("Creating new thread.");
