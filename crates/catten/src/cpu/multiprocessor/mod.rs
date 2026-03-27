50f32361c9912bac58f973d4d8416586422a1eeb
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-03-27 02:26:45 -0400

    Refactor interrupt depth management by removing LogicalProcessor struct and utilizing a static PerLp INT_DEPTH variable in line with the design decision to not have any singular structure for LP local data, instead locating all such data in the relevent subsystems.
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- crates/catten/src/cpu/multiprocessor/mod.rs
+++ crates/catten/src/cpu/multiprocessor/mod.rs
@@ -1,8 +1,38 @@
 //! # Multi-Processor Management
+
+use core::sync::atomic::{AtomicU16, Ordering};
+
+use spin::Lazy;
+
+use crate::common::sync::PerLp;
 pub mod cpu_topology;
 pub mod ipi;
-pub mod lp;
 pub mod startup;
+
+/// The current interrupt depth for each LP. This is used primarily to avoid self deadlocks.
+static INT_DEPTH: Lazy<PerLp<AtomicU16>> = Lazy::new(|| PerLp::new(|| AtomicU16::new(0)));
+
+#[unsafe(no_mangle)]
+pub extern "C" fn increment_interrupt_depth() {
+    let int_depth = &INT_DEPTH
+        .try_get_mut()
+        .expect("Failed to get mutable reference to PerLp<AtomicU16> for interrupt depth");
+    int_depth.fetch_add(1, Ordering::AcqRel);
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn decrement_interrupt_depth() {
+    let int_depth = &INT_DEPTH
+        .try_get_mut()
+        .expect("Failed to get mutable reference to PerLp<AtomicU16> for interrupt depth");
+    int_depth.fetch_sub(1, Ordering::AcqRel);
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn get_interrupt_depth() -> u16 {
+    let int_depth = &INT_DEPTH
+        .try_get_mut()
+        .expect("Failed to get mutable reference to PerLp<AtomicU16> for interrupt depth");
+    int_depth.load(Ordering::Acquire)
+}
 
 #[inline]
 pub fn get_lp_count() -> u32 {
