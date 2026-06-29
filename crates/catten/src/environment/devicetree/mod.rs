2ed1d17f2573e6958f444b6c9eb94dd542efb286
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-06-28 23:34:58 -0400

    Removed unimplemented environment modules

--- crates/catten/src/environment/devicetree/mod.rs
+++
@@ -1,4 +0,0 @@
-//! # Devicetree
-//!
-//! This module provides facilities for parsing flattened device trees (FDTs) and abstracts them to
-//! the common interface expected by the rest of the kernel.
