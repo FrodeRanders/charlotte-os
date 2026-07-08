8bad4ec4f1fd5d8aea9857e30d6f0167041df4b3
Mohit D Patel <mdpatel.csecon@outlook.com>
2026-07-08 12:37:33 -0400

    Refactor IoApic structure and implement read/write methods for 32-bit and 64-bit operations

--- crates/catten/src/cpu/isa/x86_64/interrupts/ioapic/mod.rs
+++ crates/catten/src/cpu/isa/x86_64/interrupts/ioapic/mod.rs
@@ -1,11 +1,40 @@
-#[repr(C, packed)]
-pub struct IoApic {
-    ioregsel: IoReg32,
-    ioregwin: IoReg32,
+struct IoApicOverlay {
+    ioregsel: u32,
+    ioregwin: u32,
 }
 
-impl From<NonNull<IoApic>> for IoApic {
+impl IoApicOverlay {
+    pub fn read32(&self, index: u32) -> u32 {
+        unsafe {
+            core::ptr::write_volatile(&self.ioregsel, index);
+            core::ptr::read_volatile(&self.ioregwin)
+        }
+    }
+
+    pub fn write32(&mut self, index: u32, value: u32) {
+        unsafe {
+            core::ptr::write_volatile(&mut self.ioregsel, index);
+            core::ptr::write_volatile(&mut self.ioregwin, value);
+        }
+    }
+
+    pub fn read64(&self, index: u32) -> u64 {
+        let low = self.read32(index) as u64;
+        let high = self.read32(index + 1) as u64;
+        (high << 32) | low
+    }
+
+    pub fn write64(&mut self, index: u32, value: u64) {
+        self.write32(index, value as u32);
+        self.write32(index + 1, (value >> 32) as u32);
+    }
+}
+
+#[repr(transparent)]
+pub struct IoApic(*mut IoApicOverlay);
 
 impl IoApic {
-
+    fn get_id(&self) -> u32 {
+        unsafe { (*self.0).read32(0) >> 24 }
+    }
 }
